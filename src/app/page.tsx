90d4db896d41af38cd3e7d1a5fac14f911f046ca
Brandon Kocur <kocurbrandon@gmail.com>
2023-06-24 14:07:22 -0400

    better responsiveness on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,19 @@ import { ReactNode } from "react";
 
 export default function Home() {
   return (
-    <div className="mx-auto flex max-w-4xl gap-8">
-      <aside className="sticky top-0 max-h-screen w-1/2 py-24">
+    <div className="mx-auto flex max-w-4xl flex-col gap-8 px-6 sm:flex-row">
+      <aside className="top-0 max-h-screen py-10 sm:sticky sm:w-1/2 sm:py-24">
         <h1 className="mb-2 bg-gradient-to-r from-blue-500 to-blue-700 bg-clip-text py-1 text-4xl font-black tracking-tight text-transparent dark:from-cyan-400 dark:to-blue-500 sm:text-5xl">
           Brandon Kocur
         </h1>
-        <h2 className="mb-10 text-lg font-bold text-neutral-800 dark:text-neutral-200">
+        <h2 className="mb-4 text-lg font-bold text-neutral-800 dark:text-neutral-200 sm:mb-10">
           Full Stack Software Engineer
         </h2>
         <p className="text-neutral-600 dark:text-neutral-300">
           Passion for UI interfaces, DX tooling, and Open Source.
         </p>
       </aside>
-      <main className="ml-auto flex w-1/2 flex-col gap-8 py-24">
+      <main className="ml-auto flex flex-col gap-8 pb-10 sm:w-1/2 sm:py-24">
         <section>
           <div className="flex flex-col px-4">
             <FolderGit2 className="mb-4" />
