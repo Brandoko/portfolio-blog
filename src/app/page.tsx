4bd7a4be1959af02189ace5e5ae8e5a1b1833be3
Brandon Kocur <kocurbrandon@gmail.com>
2023-06-25 10:33:00 -0400

    new favicon and add blog tutorial code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { getBlogs } from "@/lib/get-blogs";
 import { FolderGit2, MessageSquare } from "lucide-react";
 import { Url } from "next/dist/shared/lib/router/router";
 import Link from "next/link";
-import { ReactNode } from "react";
+import { HTMLAttributeAnchorTarget, ReactNode } from "react";
 
 export default function Home() {
   return (
@@ -22,22 +22,33 @@ export default function Home() {
         </p>
       </aside>
       <main className="ml-auto flex flex-col gap-8 pb-10 sm:w-1/2 sm:py-24">
-        <section>
-          <div className="flex flex-col px-4">
-            <FolderGit2 className="mb-4" />
-            <h2 className="mb-4 text-lg font-bold">Projects</h2>
-          </div>
-          <div className="flex flex-col gap-4">
-            <Project
-              title="Portfolio/Blog"
-              description="Open Source Portfolio/Blog built with Next.js (App Router) and MDX for fast and clean blogging."
-              link="https://github.com/Brandoko/portfolio-blog"
-            />
-          </div>
-        </section>
+        <Projects />
         <Blogs />
       </main>
     </div>
+  );
+}
+
+function Projects() {
+  return (
+    <section>
+      <div className="flex flex-col px-4">
+        <FolderGit2 className="mb-4" />
+        <h2 className="mb-4 text-lg font-bold">Projects</h2>
+      </div>
+      <div className="flex flex-col gap-4">
+        <Project
+          title="My Personal Website/Blog"
+          description="Built with Next.js (App Router) and MDX for easy and clean blogging."
+          link="https://github.com/Brandoko/portfolio-blog"
+        />
+        <Project
+          title="Dev Blog Starter"
+          description="Code from the Next.js + MDX Blog tutorial."
+          link="https://github.com/Brandoko/nextjs-mdx-blog-starter"
+        />
+      </div>
+    </section>
   );
 }
 
@@ -51,7 +62,7 @@ function Project({
   link: string;
 }) {
   return (
-    <ClickableContainer href={link}>
+    <ClickableContainer href={link} target="_blank">
       <h3 className="mb-2 font-bold">{title}</h3>
       <p className="text-neutral-600 dark:text-neutral-300">{description}</p>
     </ClickableContainer>
@@ -102,13 +113,16 @@ function BlogPost({
 function ClickableContainer({
   children,
   href,
+  target,
 }: {
   children: ReactNode;
   href: Url;
+  target?: HTMLAttributeAnchorTarget;
 }) {
   return (
     <Link
       href={href}
+      target={target}
       className="rounded-lg border border-neutral-200 bg-neutral-50 p-4 hover:bg-neutral-100 dark:border-neutral-600 dark:bg-neutral-800 dark:hover:bg-neutral-700"
     >
       {children}
