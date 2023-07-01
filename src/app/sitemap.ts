cc953a2b995b9237ce59fe451fb420495d5d3094
Brandon Kocur <kocurbrandon@gmail.com>
2023-07-01 13:43:31 -0400

    add return type to sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,7 @@
 import { getBlogs } from "@/lib/get-blogs";
+import { MetadataRoute } from "next";
 
-export default async function sitemap() {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getBlogs();
   const blogs = posts.map((post) => ({
     url: `https://brandonkocur.dev/blogs/${post.slug}`,
