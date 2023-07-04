dcfef6badf6b424acdcb55c015dd541db2d296c7
Brandon Kocur <kocurbrandon@gmail.com>
2023-07-03 21:17:10 -0400

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -51,6 +52,7 @@ export default function RootLayout({
         </div>
 
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
