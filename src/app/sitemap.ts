076fc8a638e509f263e215f3e7097af04fc71b31
Brandon Kocur <kocurbrandon@gmail.com>
2023-07-08 09:28:45 -0400

    update sitemap to include www

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,11 +4,11 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getBlogs();
   const blogs = posts.map((post) => ({
-    url: `https://brandonkocur.dev/blogs/${post.slug}`,
+    url: `https://www.brandonkocur.dev/blogs/${post.slug}`,
   }));
 
   const routes = [""].map((route) => ({
-    url: `https://brandonkocur.dev${route}`,
+    url: `https://www.brandonkocur.dev${route}`,
   }));
 
   return [...routes, ...blogs];
